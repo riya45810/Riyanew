cda27814acaae8793f7af8bb66efca74a161aa46
Riya Fathima <riya.fathima@experionglobal.local>
2023-02-14 17:04:15 +0530

    new

--- Reverse/Program.cs
+++ Reverse/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the name: ");
+            Console.WriteLine("Enter name: ");
             string name = Console.ReadLine();
             string rev = string.Empty;
             for (int i= name.Length-1; i>=0; i--)
