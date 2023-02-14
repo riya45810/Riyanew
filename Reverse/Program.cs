fbd172896ad1cb737e7af03368871e9d67482641
Riya Fathima <riya.fathima@experionglobal.local>
2023-02-14 16:57:30 +0530

    msg

---
+++ Reverse/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name: ");
+            string name = Console.ReadLine();
+            string rev = string.Empty;
+            for (int i= name.Length-1; i>=0; i--)
+            {
+                rev +=name[i];
+            }
+
+            Console.WriteLine($"Reverse is {rev}");
+            
+            
+            
+        }
+    }
+}
